cc9b1a003198d9c1087dba35f56e74df920720d3
chimichubanga <chimichubanga@vk.com>
2024-07-24 12:11:41 +0300

    Зарегистрированы новые cogs для всех обновленных разделов.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # main.py
 import telebot
 from config import TOKEN
-from cogs import main_menu, navigation, homework_list, webinar_records, bot_info
+from cogs import main_menu, navigation, homework_list, webinar_records, bot_info, help
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -11,6 +11,7 @@ navigation.register_handlers(bot)
 homework_list.register_handlers(bot)
 webinar_records.register_handlers(bot)
 bot_info.register_handlers(bot)
+help.register_handlers(bot)
 
 if __name__ == '__main__':
     print("Bot is running...")
